9ab0cda84e64329b6a822c055b88dd686e810929
User <User@DESKTOP-3APE4MB>
2022-10-13 17:11:39 +0300

    Add project files.

---
+++ 03. Point on Segment/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace _03._Point_on_Segment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            var tochka1 = int.Parse(Console.ReadLine());
+            var tochka2 = int.Parse(Console.ReadLine());
+            var tochka3 = int.Parse(Console.ReadLine());
+
+
+            var measure = 0;
+            if (Math.Abs(tochka3 - tochka1) <= Math.Abs(tochka3 - tochka2))
+            {
+                measure = Math.Abs(tochka3 - tochka1);
+            }
+            else {measure = Math.Abs(tochka3 - tochka2); }
+            if (tochka1 > tochka2)
+            {
+                var oldTochka2 = tochka2;
+                tochka2 = tochka1;
+                tochka1 = oldTochka2;
+            }
+            for (int i = tochka1; i <= tochka2; i++)
+            {
+                if (i == tochka3)
+                {
+                    Console.WriteLine("in");
+                    Console.WriteLine(measure);
+                    return;
+                }
+            }
+            Console.WriteLine("out"); 
+            Console.WriteLine(measure);
+
+
+
+
+
+
+
+
+
+            //if(tochka1 > tochka2)
+            //{
+            //    var oldTochka2 = tochka2;
+            //    tochka2 = tochka1;
+            //    tochka1 = oldTochka2;
+            //}
+            //if(tochka3 >= tochka1 && tochka3 <= tochka2)
+            //{
+            //    Console.WriteLine("in");
+            //    if(Math.Abs(tochka3 - tochka1) <= Math.Abs(tochka3 - tochka2))
+            //    {
+            //        Console.WriteLine(Math.Abs(tochka3 - tochka1));
+            //    }
+            //    else { Console.WriteLine(Math.Abs(tochka3 - tochka2)); }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("out");
+            //    if (Math.Abs(tochka3 - tochka1) <= Math.Abs(tochka3 - tochka2))
+            //    {
+            //        Console.WriteLine(Math.Abs(tochka3 - tochka1));
+            //    }
+            //    else { Console.WriteLine(Math.Abs(tochka3 - tochka2)); }
+            //}
+
+        }
+    }
+}
---
+++ Пробно/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _12._Generate_Rectangles
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+
+            int n = int.Parse(Console.ReadLine());
+            int minArea = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            for (int x1Left = -n; x1Left < n; x1Left++)
+               
+
+            {
+                for (int y1Top = -n; y1Top < n; y1Top++)
+                   
+                {
+                    for (int x2Right = x1Left+1; x2Right <= n; x2Right++)
+                    {
+                        for (int y2Bottom = y1Top+1; y2Bottom <= n; y2Bottom++)
+                        {
+                            int xSide = Math.Abs(x1Left - x2Right);
+                            int ySide = Math.Abs(y1Top - y2Bottom);
+                            int area = ySide * xSide;
+                            if (area >= minArea)
+                            {
+                                count++;
+                                Console.WriteLine($"({x1Left}, {y1Top}) ({x2Right}, {y2Bottom}) -> {area}");
+                            }
+                        }
+                    }
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
